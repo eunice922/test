f3688f7e355f18fc5754a85dd94c10b7885b1e24
Yoonkyung Lee <dnib@naver.com>
2012-04-19 20:36:57 +0900

    7 commit

---
+++ cache_manifest.php
@@ -0,0 +1,35 @@
+<?PHP
+header('Content-Type: text/cache-manifest');
+echo "CACHE MANIFEST\n\n";
+
+#version 1.0
+echo "CACHE:\n";
+
+echo "assets/js/jquery-1.6.2.min.js\n";
+echo "../_Library/plugins/jquery-ui-1.8.16.custom.min.js\n";
+echo "assets/js/magictouch.js\n";
+echo "book.js\n";
+echo "assets/css/book.css\n";
+echo "assets/images/icon.png\n";
+echo "assets/images/startup.png\n";
+echo "assets/data/book.xml\n";
+echo "assets/video/volcano.mp4\n";
+echo "assets/video/oliveCream.mp4\n";
+echo "assets/images/page1.jpg\n";
+echo "assets/images/page2.jpg\n";
+echo "assets/images/page3.jpg\n";
+echo "assets/images/page4.jpg\n";
+echo "assets/images/page5.jpg\n";
+echo "assets/images/page6.jpg\n";
+echo "assets/images/page7.jpg\n";
+echo "assets/images/page8.jpg\n";
+echo "assets/images/arrowLeft.png\n";
+echo "assets/images/arrowRight.png\n";
+echo "assets/images/pauseBtn.png\n";
+echo "assets/images/playBtn.png\n";
+
+echo "NETWORK:\n\n";
+ 
+echo "FALLBACK:\n";
+echo "assets/images/startup.png\n";
+?>
